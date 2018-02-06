f0d1dc15b32cd2fe8f52d545dac0ee6f8a5f46df
litengbin <874214733@qq.com>
2018-02-07 01:12:32 +0800

    test

--- src/main/java/com/ltb/jase/App.java
+++ src/main/java/com/ltb/jase/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-            System.out.println( "Hello World! ltbltbltb" );
+            System.out.println( "Hello World! ltbltbltbltb" );
     }
 }
